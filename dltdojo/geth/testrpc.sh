646e7f8cd5da3460860c4456ad3048470f3ce9c4
dltdojo <y12studio@gmail.com>
2017-08-20 08:51:22 +0800

    update geth testrpc mode

--- dltdojo/geth/testrpc.sh
+++ dltdojo/geth/testrpc.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 geth --dev --datadir /root/.ethereum/ --rpc --rpcapi miner,admin,db,personal,eth,net,web3 --rpccorsdomain="*" --rpcaddr="0.0.0.0" --ws --wsaddr="0.0.0.0" --wsorigins "*" &
 sleep 2
-geth --exec 'personal.newAccount("pass");miner.start();personal.unlockAccount(eth.accounts[0], "pass", 3600);' attach
+geth --exec 'personal.newAccount("pass");miner.start();personal.unlockAccount(eth.accounts[0], "pass", 7200);' attach
 sleep 2
+geth --exec 'personal.newAccount("pass");personal.unlockAccount(eth.accounts[1], "pass", 7200);' attach
+sleep 2
+geth --exec 'personal.newAccount("pass");personal.unlockAccount(eth.accounts[2], "pass", 7200);' attach
 tail -f /dev/null
